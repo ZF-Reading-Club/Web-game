367b7c9f2cfc62fb23d541885c22c61189c5a2a9
Kamila Hantová <kamila.hantova@gmail.com>
2025-02-13 21:31:39 +0100

    prepare table interface

---
+++ database_model/model/database_context.py
@@ -0,0 +1,24 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+class DatabaseContext:
+    HOST= ""
+    DATABASE = "web_game"
+    USER = "admin"
+    PASSWORD = "admin"
+    
+    Base = declarative_base()
+    
+    def __init__(self) -> None:
+        """Initialize database connection."""
+        url=f'postgresql://{self.USER}:{self.PASSWORD}@{self.HOST}/{self.DATABASE}'
+        self.engine = create_engine(url)
+        Session = sessionmaker(bind=self.engine)
+        self.session=Session()
+        
+    def initialize_database(self):
+        self.Base.metadata.create_all(self.engine)
+        
