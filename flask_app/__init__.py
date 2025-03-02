40191cd67d4d6637bc8afb61566bd46330ed63b8
OFCIHLAVA <siska.rotto@gmail.com>
2025-03-02 23:03:11 +0100

    Wip

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -3,13 +3,41 @@
 Code here gets executed when the app object form here gets imported anywhere.
 """
 
+import os
+
 from flask import Flask
 from flask_cors import CORS
+
+from flask_jwt_extended import JWTManager
 
 # Create a new Flask app
 # App needs to be created here before importing routes to avoid circular imports and to ensure that
 # the app object is available to the routes.
-app = Flask(__name__)
+
+# Determine the absolute path of the current file (app/__init__.py)
+app_dir = os.path.dirname(os.path.abspath(__file__))
+print("App directory path:", app_dir)
+# Build the absolute path to the templates folder (assuming it's at project_root/frontend/templates)
+template_path = os.path.join(app_dir, "..", "frontend", "templates")
+print("Computed template folder path:", template_path)
+
+# Print the computed path for debugging
+print("Computed template folder path:", os.path.abspath(template_path))
+
+
+app = Flask(
+    __name__,
+)
 CORS(app)
+
+# JWT settings
+app.config["JWT_SECRET_KEY"] = "super"
+app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
+app.config["JWT_COOKIE_SECURE"] = False
+app.config["JWT_ACCESS_TOKEN_PATH"] = "/"
+app.config["JWT_COOKIE_SAMESITE"] = "Strict"
+app.config["JWT_COOKIE_CSRF_PROTECT"] = False
+
+jwt = JWTManager(app)
 
 from flask_app import routes
--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,13 +1,28 @@
 """This is the routes file for the flask app. It contains all the routes for the app.
 """
 
-from flask import jsonify
+from flask import jsonify, request, render_template
 
 from flask_app import app
 from flask_app.server.server_logic import health_check_response
 
 
+from flask_jwt_extended import jwt_required, create_access_token, set_access_cookies
+
+
+@app.route("/")
+def index() -> str:
+    """Index endpoint that returns a simple message.
+
+    Returns:
+        str: A simple message.
+    """
+    app.logger.info("Handling request to /")
+    return render_template("sign_in.html")
+
+
 @app.route("/api/test", methods=["GET"])
+@jwt_required()
 def health_check() -> dict:
     """Test endpoint that returns status and timestamp.
 
@@ -16,3 +31,29 @@ def health_check() -> dict:
     """
     app.logger.info("Handling request to /api/test")
     return jsonify(health_check_response())
+
+
+@app.route("/api/login", methods=["POST"])
+def sign_in() -> dict:
+    """Test endpoint to sign in.
+
+    Returns:
+        dict: A response dictionary from health_check_response().
+    """
+    app.logger.info("Handling request to /api/login")
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+    if username:
+        if username == "ondra" and password == "123":
+            app.logger.info(f"Valid credentials for user {username}")
+            token = create_access_token(identity=username)
+            app.logger.info(f"Generated token for user {token}")
+            response = jsonify(login=True, message="Login successful")
+            app.logger.info(f"Generated response {response}")
+            set_access_cookies(response, token)
+            return response, 200
+        else:
+            return jsonify(success=False, message="Invalid credentials"), 401
+    else:
+        return jsonify(success=False, message="Missing credentials"), 400
