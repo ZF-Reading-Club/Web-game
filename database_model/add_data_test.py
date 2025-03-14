277518e42626bb52d8eee1e9dfde0cb0f3a0d9a2
Kamila Hantová <kamila.hantova@gmail.com>
2025-03-14 20:10:28 +0100

    Enhance AddDataTest class with detailed docstrings for better code documentation.

--- database_model/add_data_test.py
+++ database_model/add_data_test.py
@@ -9,10 +9,26 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 class AddDataTest:
     def __init__(self) -> None:
+        """
+        Initializes the instance with default values.
+        Attributes:
+            data (dict): A dictionary to store data.
+            user_id (str): A string to store the user ID.
+        """
+        
         self.data = {}
         self.user_id = ""
        
     def set_test_data(self):
+        """
+        Sets test data for the instance.
+        This method populates the `data` attribute with predefined test values:
+        - name
+        - money
+        - territory
+        - buildings
+        """
+        
         self.data["name"]="Hoa"    
         self.data["money"]="123"    
         self.data["territory"]="none"   
@@ -20,6 +36,16 @@ class AddDataTest:
         print(self.data)
         
     def add_data_into_database(self):
+        """
+        Adds data into the database.
+        This method initializes the database context, creates a session, and attempts to add a new user record.
+        Attributes:
+            self.data (dict): A dictionary containing user data to be added to the database.
+            self.user_id (int): The ID of the user added to the database.
+        Returns:
+            None
+        """
+        
         database_context = DatabaseContext() 
         database_context.initialize_database()
         session = database_context.session
@@ -37,6 +63,13 @@ class AddDataTest:
             print(is_in_database.first().id)    
         
     def is_in_database(self, session):
+        """
+        Checks if the current data exists in the UserTable of the database.
+        Args:
+            session (Session): The SQLAlchemy session used to query the database.
+        Returns:
+            Query: The SQLAlchemy query object that can be used to check if the data exists in the database.
+        """
         query = session.query(UserTable).filter_by(**self.data)
         return query    
     
